19ca246d1910d38bdb6c2c1f064a095c85797f0c
Johnny Doe <bsandr95@gmail.com>
2018-05-04 02:26:35 -0700

    Replaced switch statement with if statements.

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -38,8 +38,8 @@ public class GT4500 implements SpaceShip {
 
     boolean firingSuccess = false;
 
-    switch (firingMode) {
-      case SINGLE:
+
+      if(firingMode == FiringMode.SINGLE)
         if (wasPrimaryFiredLast) {
           // try to fire the secondary first
           if (! secondaryTorpedoStore.isEmpty()) {
@@ -74,16 +74,12 @@ public class GT4500 implements SpaceShip {
             // if both of the stores are empty, nothing can be done, return failure
           }
         }
-        break;
 
-      case ALL:
+      else if (firingMode == FiringMode.ALL)
         // try to fire both of the torpedo stores
         if (! primaryTorpedoStore.isEmpty() && ! secondaryTorpedoStore.isEmpty()) {
             firingSuccess = (primaryTorpedoStore.fire(1) || secondaryTorpedoStore.fire(1));
           }
-
-        break;
-    }
 
     return firingSuccess;
   }
